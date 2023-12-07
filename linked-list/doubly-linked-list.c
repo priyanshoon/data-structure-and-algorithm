954a4a0445e46aaa17a5f509291c9fe5e3d5a760
priyanshoon <guptapriyanshu973@gmail.com>
2023-12-07 20:49:45 +0530

    hmm

--- linked-list/doubly-linked-list.c
+++ linked-list/doubly-linked-list.c
@@ -1,6 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node_ {
+    int data;
+    struct node_ *prv;
+    struct node_ *next;
+} node;
+
+void traverse(node *ptr) {
+    node *temp = ptr;
+    printf("circular linked list: ");
+    while (ptr != NULL) {
+        printf("%d -> ", ptr->data);
+        ptr = ptr->next;
+
+    }
+    printf("END LINKED LIST\n");
+}
+
+void reverse(node *ptr) {
+    node *temp = ptr;
+    printf("circular linked list: ");
+    while (ptr != NULL) {
+        printf("%d -> ", ptr->data);
+        ptr = ptr->next;
+
+    }
+    printf("END LINKED LIST\n");
+}
+
 int main(int argc, char *argv[]) {
+    node *head = (node*)malloc(sizeof(node));
+    node *a1 = (node*)malloc(sizeof(node));
+    node *a2 = (node*)malloc(sizeof(node));
+    node *a3 = (node*)malloc(sizeof(node));
+
+    head->prv = NULL;
+    head->data = 10;
+    head->next = a1;
+
+    a1->prv = head;
+    a1->data = 20;
+    a1->next = a2;
+
+    a2->prv = a1;
+    a2->data = 30;
+    a2->next = a3;
+
+    a3->prv = a2;
+    a3->data = 40;
+    a3->next = NULL;
+
+    traverse(head);
     return 0;
 }
--- stacks/main_3.c
+++ stacks/main_3.c
@@ -1,7 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    // infix (a + b), prefix (+ab), postfix (ab+)
+struct stack {
+    int size;
+    int top;
+    int *arr;
+};
+
+int isEmpty(struct stack *ptr) {
+    if (ptr->top == -1) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int isFull(struct stack *ptr) {
+    if (ptr->top == ptr->size - 1) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void push(struct stack *ptr, int val) {
+    if(isFull(ptr)) {
+        printf("The stack is full\n");
+    } else {
+        ptr->top++;
+        ptr->arr[ptr->top] = val;
+    }
+}
+
+int pop(struct stack *ptr) {
+    if(isEmpty(ptr)) {
+        printf("The stack is already empty dumbo!\n");
+        return -1;
+    } else {
+        int val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+
+int peek(struct stack *ptr, int i) {
+    int arrIndex = ptr->top - i + 1;
+    if(arrIndex < 0) {
+        printf("Not the valid position for the stack\n");
+        return -1;
+    } else {
+        return ptr->arr[arrIndex];
+    }
+}
+
+int main() {
+    struct stack *s;
+
+    s->size = 80;
+    s->top = -1;
+    s->arr = (int*)malloc(s->size * sizeof(int));
+
+    if (isEmpty(s)) {
+        printf("The stack is empty\n");
+    } else {
+        printf("The stack is not empty\n");
+    }
+
+    s->arr[0] = 7;
+    s->top++;
+
+    if (isEmpty(s)) {
+        printf("The stack is empty\n");
+    } else {
+        printf("The stack is not empty\n");
+    }
+
     return 0;
 }
