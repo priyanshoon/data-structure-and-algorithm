1805e1ba792da6beafca041e77a1ef4e5b9bf616
priyanshoon <62681950+priyanshoon@users.noreply.github.com>
2024-02-11 14:01:52 +0530

    tree

---
+++ tree/tree.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct treenode {
+    int value;
+    struct treenode* left;
+    struct treenode* right;
+} treenode;
+
+treenode* createTreeNode() {
+
+}
+
+void preOrderTraversal() {
+
+}
+
+void inOrderTraversal() {
+
+}
+
+void postOrderTraversal() {
+
+}
+
+int main() {
+
+    return 0;
+}
