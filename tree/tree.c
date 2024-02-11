b08f099756fb4c545241cba79a03259b3378bb63
priyanshoon <62681950+priyanshoon@users.noreply.github.com>
2024-02-11 18:33:46 +0530

    tree preorder traversal

--- tree/tree.c
+++ tree/tree.c
@@ -7,23 +7,47 @@ typedef struct treenode {
     struct treenode* right;
 } treenode;
 
-treenode* createTreeNode() {
-
+treenode* createTreeNode(int value) {
+    treenode *tree;
+    tree = (treenode*)malloc(sizeof(treenode));
+    tree->value = value;
+    tree->left = NULL;
+    tree->right = NULL;
+    return tree;
 }
 
-void preOrderTraversal() {
-
+void preorder(treenode *root) {
+    if(root != NULL) {
+        printf("%d\n", root->value);
+        preorder(root->left);
+        preorder(root->right);
+    }
 }
 
-void inOrderTraversal() {
-
-}
-
-void postOrderTraversal() {
+void postorder(treenode *root) {
 
 }
 
 int main() {
+    treenode *p = createTreeNode(4);
+    treenode *p1 = createTreeNode(1);
+    treenode *p2 = createTreeNode(6);
+    treenode *p3= createTreeNode(5);
+    treenode *p4= createTreeNode(2);
+
+    // Finally tree looks like this 
+    //      4
+    //     / \
+    //    1   6
+    //   / \
+    //  5   2
+
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    preorder(p);
 
     return 0;
 }
